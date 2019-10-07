e9f012a660ffe869640cdc867076384e89d520e6
Traxler <fabian.traxler@capgemini.com>
2019-10-07 16:49:39 +0200

    Database Structure  added + python packages

--- src/Backend/Python_API/modules/db_handler.py
+++ src/Backend/Python_API/modules/db_handler.py
@@ -1,2 +1,33 @@
-def execute_statement(SQL_statement):
-    return
+import psycopg2
+import json
+
+
+class DB_Handler():
+    def __init__(self, db_config_file):
+        with open(db_config_file) as config_file:
+            config = json.load(config_file)
+    
+        self.connection = psycopg2.connect(
+            database=config['Database'], 
+            user=config['User'], 
+            password=config['Password'], 
+            host=config['Host'], 
+            port=config['Port'])
+        
+        self.cursor = self.connection.cur()
+
+
+    def get_results(self, SQL_statement):
+        self.cursor.execute(SQL_statement)
+        rows = self.cursor.fetchall()
+        return rows
+    
+    def get_result(self, SQL_statement):
+        self.cursor.execute(SQL_statement)
+        row = self.cursor.fetchone()
+        return row
+
+    def commit_statement(self, SQL_statement):
+        self.cursor.execute(SQL_statement)
+        self.connection.commit()
+        return "Success"
--- src/Backend/Python_API/modules/group.py
+++ src/Backend/Python_API/modules/group.py
@@ -1,20 +1,62 @@
-import db_handler as DB_Handler
+from collections import defaultdict 
 
-def get_disciplines(group_name):
-    return
 
-def get_atheltes(group_name):
-    return
+class Group_Handler():
+    def __init__(self, DB_Handler):
+        self.DB_Handler = DB_Handler
+        self.group_disciplines = {
+            "Decathlon": ['100 Meter', 'Weitsprung', 'Kugel Stoßen', 'Hochsprung', '400 Meter', '110 Meter', 'Diskus', 'Stabhochsprung', 'Speerwurf', '1500 Meter'],
+            "Pethathlon": ['100 Meter', 'Weitsprung', 'Hochsprung', 'Speerwurf', '1200 Meter'],
+            "Triathlon": ['60 Meter', 'Weitsprung', 'Schlagball']
+        }
 
-def get_score(group_name):
-    return
+    def get_available_groups(self):
+        """"Returns Dict with Categories as keys and list of Groups as values"""
 
-def create(group_name, type):
-    return 'Group Created'
+        query = 'SELECT name, category FROM Groups;'
+        rows = self.DB_Handler.get_results(query)
+        result = defaultdict(list)
+        for row in rows:
+            result[row[1]].append(row[0])
+        return result
 
-def create_password(group_name):
-    return
+    def get_disciplines(self, group_name):
+        """Returns List of all Disciplines for a given Group"""
 
-def get_status(group_name):
-    return
+        query = 'SELECT type FROM Groups WHERE name = {}'.format(group_name)
+        row = self.DB_Handler.get_result(query)
+        type_ = row[0]
+        return self.group_disciplines[type_]
+
+    def get_athletes(self, group_name):
+        """Returns List of all Athletes for a given Group"""
+
+        query = 'SELECT number, first_name, last_name FROM Athletes WHERE group_name = {}'.format(group_name)
+        rows = self.DB_Handler.get_results(query)
+        athletes = {}
+        for row in rows:
+            athletes[row[0]] = {}
+            athletes[row[0]]['Vorname'] = row[1]
+            athletes[row[0]]['Nachname'] = row[2]
+            athletes[row[0]]['Punkte'] = 0
+        
+        query = 'SELECT athlete_number, points FROM Achievements WHERE group_name = {}'.format(group_name)
+        rows = self.DB_Handler.get_results(query)
+        for row in rows:
+            athletes[row[0]]['Punkte'] += int(row[1])
+
+        return athletes
+
+    def get_score(self, group_name):
+        
+        return
+
+    def create(self, group_name, type):
+        return 'Group Created'
+
+    def create_password(self, group_name):
+        return
+
+    def get_status(self, group_name):
+        return
 
